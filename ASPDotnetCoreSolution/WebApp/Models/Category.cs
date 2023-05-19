d21cedc58ac0cceb3f82ffc1182b8d062d0e89fd
Priscilla Baah <pyfbaah@gmail.com>
2023-05-19 09:21:02 +0000

    add catgory model

---
+++ ASPDotnetCoreSolution/WebApp/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models
+{
+    public class Category
+    {
+        [Key]
+        // Key data annotation(attribute) sets the Id column as the primary key and identity column
+        public int Id { get; set; }
+        [Required]
+        public int Name { get; set; }
+        public int DisplayOrder { get; set; }
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+        // sets DateTime.Now as default value
+
+    }
+}
