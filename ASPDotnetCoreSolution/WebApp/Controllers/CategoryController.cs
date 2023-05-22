90bd43eff874c0348118c3572788e7666d4f74e7
Priscilla Baah <pyfbaah@gmail.com>
2023-05-22 12:52:46 +0000

    add object list from category db

---
+++ ASPDotnetCoreSolution/WebApp/Controllers/CategoryController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Data;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        // why is this constructor necessary??
+
+        public IActionResult Index()
+        {
+            //var objCategoryList = _db.Categories.ToList();
+            IEnumerable<Category> objCategoryList = _db.Categories;
+            return View(objCategoryList);
+        }
+    }
+}
