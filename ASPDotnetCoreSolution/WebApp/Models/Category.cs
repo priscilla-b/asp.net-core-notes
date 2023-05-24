3635a11124b0dca824445bba5c9ad6bcf3040383
Priscilla Baah <pyfbaah@gmail.com>
2023-05-24 10:02:24 +0000

    add display name for model field

--- ASPDotnetCoreSolution/WebApp/Models/Category.cs
+++ ASPDotnetCoreSolution/WebApp/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebApp.Models
 {
@@ -9,6 +10,7 @@ namespace WebApp.Models
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
+        [DisplayName("Display Order")]
         public int DisplayOrder { get; set; }
         public DateTime CreatedDateTime { get; set; } = DateTime.Now;
         // sets DateTime.Now as default value
